16634e89f105bbb4061ecd060a82cc82707e664c
XavierF-C <xavier_f-c@hotmail.com>
2022-04-13 15:11:32 -0400

    XFC Added tooltip for viz 3

--- src/scripts/colors.js
+++ src/scripts/colors.js
@@ -1,5 +1,6 @@
-export const TAUX_AVANCE = '#3fde0A'
-export const TAUX_PONCTUEL = '#888888'
-export const TAUX_RETARD = '#f74242'
 export const TAUX_AVANCE_VIZ4 = '#39a156'
 export const TAUX_RETARD_VIZ4 = '#d52222'
+
+export const TAUX_AVANCE_VIZ3 = TAUX_AVANCE_VIZ4
+export const TAUX_PONCTUEL_VIZ3 = '#cccccc'
+export const TAUX_RETARD_VIZ3 = TAUX_RETARD_VIZ4
