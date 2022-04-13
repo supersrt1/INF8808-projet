6d7a5965de4f4b9470872c5826181c918fd423e2
marco novaes <marcool_98@hotmail.com>
2022-04-13 09:52:52 -0400

    Added colors viz4 as constants

--- src/scripts/colors.js
+++ src/scripts/colors.js
@@ -1,3 +1,6 @@
 export const TAUX_AVANCE = '#3fde0A'
 export const TAUX_PONCTUEL = '#888888'
 export const TAUX_RETARD = '#f74242'
+
+export const TAUX_AVANCE_VIZ4 = '#39a156'
+export const TAUX_RETARD_VIZ4 = '#d52222'
