2b32cfb4c02f1704c62d3fdd7faf0cd107a25af9
Tristan Rioux <tristan.rioux@polymtl.ca>
2022-04-24 00:44:28 -0400

    Viz 2 legend working

--- src/modules/viz2/viz.js
+++ src/modules/viz2/viz.js
@@ -4,16 +4,19 @@ import d3Tip from 'd3-tip'
 
 let g_data = [];
 
-const id = 'map';
+const MAP_ID = 'map';
 //                      longitude, latitude
-const default_center = [-73.98353, 45.76177];
-const default_zoom = 12
+const DEFAULT_CENTER = [-73.98353, 45.76177];
+const DEFAULT_ZOOM = 12;
+const MIN_ZOOM = 10;
+const MAX_ZOOM = 16;
 
-const {map, svg} = createMap(id, default_center, default_zoom);
+const {map, svg} = createMap(MAP_ID, DEFAULT_CENTER, DEFAULT_ZOOM);
 
 function createMap(id, center, zoom) {
     const map = L.map(id).setView([center[1], center[0]], zoom);
 
+    // Other map tiles that could be used to change esthetic.
     /*const Stadia_OSMBright = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
         maxZoom: 20,
         attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
@@ -24,22 +27,22 @@ function createMap(id, center, zoom) {
 	minZoom: 0,
 	maxZoom: 20,
 	ext: 'png'
-})*/
+    })*/
     const Stamen_TonerBackground = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}{r}.{ext}', {
         attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         subdomains: 'abcd',
-        minZoom: 10,
-        maxZoom: 16,
+        minZoom: MIN_ZOOM,
+        maxZoom: MAX_ZOOM,
         ext: 'png'
     }).addTo(map);
 
     L.svg().addTo(map);
 
-    const legend = L.control({position: 'topright'})
+    /*const legend = L.control({position: 'topright'})
     legend.onAdd = (map) => {
-        return L.DomUtil.create('svg', 'viz2-legend')
+        return L.DomUtil.create('svg', 'viz2-legend123')
     }
-    legend.addTo(map);
+    legend.addTo(map);*/
 
     const overlay = d3.select(map.getPanes().overlayPane);
     const svg = overlay.select('svg')
@@ -55,27 +58,67 @@ export function updateData(filteredData) {
     //g_data = preprocess.aggregatePonctualite(filteredData.byLineDirectionDateStop, ['arret_code'])
 }
 
-function makeLegend(){
-    const LEGEND_WIDTH = 150;
-    const LEGEND_HEIGHT = 50;
-    const legendSvg = d3.select(".viz2-legend")
-        .style('width', LEGEND_WIDTH+'px')
-        .style('height', LEGEND_HEIGHT+'px');
-    legendSvg.selectAll('#background')
-        .data([null])
-        .join('rect')
-        .attr('id', 'background')
-        .attr('width', LEGEND_WIDTH)
-        .attr('height', LEGEND_HEIGHT)
-        .attr('fill', 'white')
+function makeLegend(rscale){
+    const LEGEND_WIDTH = 250;
+    const LEGEND_HEIGHT = 75;
+    const legendSvg = d3.select("#viz2-legend")
+    //.attr('width', LEGEND_WIDTH)
+    //.attr('height', LEGEND_HEIGHT)
+    .style('width', LEGEND_WIDTH+'px')
+    .style('height', LEGEND_HEIGHT+'px')
+    .style('background-color', 'white');
+
     legendSvg.selectAll('#title')
+    .data([null])
+    .join('text')
+    .attr('id', 'title')
+    .attr('text-anchor', 'start')
+    .attr('alignment-baseline', 'top')
+    .attr('fill', 'black')
+    .attr('font-weight', 'bolder')
+    .attr('x', 5)
+    .attr('y', 17)
+    .text("Légende")
+
+    const g = legendSvg.selectAll('g')
         .data([null])
+        .join('g')
+
+    const {
+        width,
+        height
+    } = legendSvg.node().getBoundingClientRect()
+
+    const SCALES = [100, 1000, 10000]
+
+    const CELL_SIZE = width/ SCALES.length;
+
+    const points = g.selectAll('circle')
+        .data(SCALES)
+        .join('circle')
+        .attr('r', d =>  rscale(d))
+        .attr("cx", (d,i) => CELL_SIZE * (i) + CELL_SIZE/2)
+        .attr("cy", (d,i) => 33.5)
+        .attr("fill", "blue")
+        .attr("opacity", 0.70)
+        .attr("stroke-width", 0)
+
+    g.selectAll('text')
+        .data(SCALES)
         .join('text')
-        .attr('id', 'title')
-        .attr('text-anchor', 'start')
-        .attr('alignment-baseline', 'middle')
-        .attr('fill', 'black')
-        .text("LEGEND")
+        .attr('text-anchor', 'middle')
+        .attr('alignment-baseline', 'top')
+        .attr('x', (d, i) => CELL_SIZE * (i) + CELL_SIZE/2)
+        .attr('y', (d) => 62.35)
+        .text(d=>d)
+
+
+    const update = () => points
+        .attr('r', d=> Math.pow(2, (map.getZoom()-DEFAULT_ZOOM)) * rscale(d))
+
+    return update;
+
+    
 }
 
 export const viz = (selection, props) => {
@@ -91,20 +134,15 @@ export const viz = (selection, props) => {
                         .rollup(v => { return {name: v[0].arret_nom, cumulatif: d3.sum(v, d => d.montants), lon: v[0].arret_Longitude, lat: v[0].arret_Latitude}})
                         .entries(data.byLineDirectionDateStop);
     
+    // Sort to have a better draw order
     groupedData.sort((a, b)=> b.value.cumulatif - a.value.cumulatif)                    
     //console.log("DATA", groupedData);
-
+/*
     groupedData.forEach((v)=> {
         v.value.area = Math.sqrt(v.value.cumulatif/Math.PI)
-    });
-
-
+    });*/
     
-
-    // LEGEND
-    makeLegend();
-    
-
+    // TOOLTIP
     const getTipContent = d => {
         return `
             <p id="viz1-tooltip-title">${d.value.name}</p>
@@ -114,11 +152,14 @@ export const viz = (selection, props) => {
     const tooltip = d3Tip().attr('class', 'd3-tip').html(getTipContent)
     svg.call(tooltip)
 
-    // POINTS
-
+    // SCALE
     const minMax = d3.extent(groupedData, d => d.value.cumulatif)
     const rscale = d3.scaleSqrt().domain(minMax).range([2, 30]);
 
+    // LEGEND
+    const legendUpdate = makeLegend(rscale);
+
+    // POINTS
     const circles = svg.selectAll('#circles')
     .data([null])
     .join('g')
@@ -127,7 +168,7 @@ export const viz = (selection, props) => {
     const points = circles.selectAll('circle')
         .data(groupedData)
         .join('circle')
-        .attr('r', d=> Math.pow(2, (map.getZoom()-default_zoom)) * rscale(d.value.cumulatif))
+        .attr('r', d=> rscale(d.value.cumulatif))
         .attr("cx", d => map.latLngToLayerPoint([d.value.lat, d.value.lon]).x)
         .attr("cy", d => map.latLngToLayerPoint([d.value.lat, d.value.lon]).y)
         .attr("fill", "blue")
@@ -142,9 +183,10 @@ export const viz = (selection, props) => {
             d3.select(this).attr("fill", "blue");});
 
     const update = () => points
-        .attr('r', d=> Math.pow(2, (map.getZoom()-default_zoom)) * rscale(d.value.cumulatif))
+        //.attr('r', d=> Math.pow(2, (map.getZoom()-DEFAULT_ZOOM)) * rscale(d.value.cumulatif))
         .attr("cx", d => map.latLngToLayerPoint([d.value.lat, d.value.lon]).x)
         .attr("cy", d => map.latLngToLayerPoint([d.value.lat, d.value.lon]).y);
 
     map.on("move", update);
+    //map.on("move", legendUpdate);
 }
